b71c8d142f7e751d1a12b0a487baa81381237bb8
jainankit0811 <ankitd0811@gmail.com>
2024-05-31 11:52:01 +0530

    update

---
+++ utils/data/educations.js
@@ -0,0 +1,20 @@
+export const educations = [
+  {
+    id: 1,
+    title: "Bachelor of Technology",
+    duration: "2023 - Present",
+    institution: "Techno India NJR Institute of Technology",
+  },
+  {
+    id: 2,
+    title: "Higher Secondary Certificate",
+    duration: "2021 - 2022",
+    institution: "Rashtra Bharti Academy School",
+  },
+  {
+    id: 3,
+    title: "Secondary School Certificate",
+    duration: "2019 - 2020",
+    institution: "Rashtra Bharti Academy School",
+  }
+]
