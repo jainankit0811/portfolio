bdb1c419ae92fd95387824dbaea63e293168d5a3
jainankit0811 <ankitd0811@gmail.com>
2024-06-02 23:48:02 +0530

    deleted previous project

--- utils/data/contactsData.js
+++
@@ -1,7 +0,0 @@
-export const contactsData = {
-  email: 'ankitd0811@gmail.com',
-  github: 'https://github.com/jainankit0811',
-  linkedIn: 'https://www.linkedin.com/in/ankit-dhanawat/',
-  twitter: 'https://twitter.com/DhanawatAn99877',
-  leetcode: "https://leetcode.com/u/ankitd0811//",
-}
